fcfd4090b0b7b82e5eb3eb3d7d268c487ceb3d22
Harold Sun <bnusunny@gmail.com>
2018-08-31 15:34:12 +0800

    test github push

--- hello.go
+++ hello.go
@@ -2,7 +2,7 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("hello world - abcdefg")
+  fmt.Println("hello world")
   fmt.Println("1+1 =", 1+1)
   fmt.Println("7.0/3.0 =", 7.0/3.0)
   fmt.Println(true && false)
